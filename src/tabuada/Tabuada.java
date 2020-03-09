95edb5b24cccb6b1f32bbc8fc98d59224097a91c
Lorenalgm <lorena@proesc.com>
2020-03-09 16:28:27 -0300

    Melhorada mensagem

--- src/tabuada/Tabuada.java
+++ src/tabuada/Tabuada.java
@@ -1,20 +1,8 @@
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tabuada;
 import java.util.Scanner;
-/**
- *
- * @author labcc
- */
+
 public class Tabuada {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
 
         Scanner tabuada = new Scanner(System.in);
@@ -25,7 +13,6 @@ public class Tabuada {
         for(int i=1; i <= 10; i++){
             System.out.println("Tabuada de"+i+"x"+numero+"="+i*numero);
         }
-        // TODO code application logic here
     }
     
 }
